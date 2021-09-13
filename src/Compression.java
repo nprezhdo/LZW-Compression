3103714e54df61239220a75142c9120ede60ce11
Alex-Shane <ashane1230@gmail.com>
2021-09-12 23:25:50 -0700

    decompress method is compiling but not working properly. Writes some but not all of the file.

--- src/Compression.java
+++ src/Compression.java
@@ -1,7 +1,9 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -10,6 +12,7 @@ import java.util.*;
 public class Compression {
 	
 	private HashMap<String, Integer> table = new HashMap<String, Integer>();
+	private ArrayList<Integer> asciiValues = new ArrayList<Integer>();
 	
 	public Compression () {
 		// adds ascii table
@@ -19,15 +22,13 @@ public class Compression {
 	}
 	
 	@SuppressWarnings("deprecation")
-	public void compress (File inputFile) throws IOException{
+	public ArrayList<Integer> compress (File inputFile) throws IOException{
 
 			BufferedReader reader = new BufferedReader (new FileReader(inputFile));
 			
 			int number = 256;
 			String current = "" + (char)reader.read();
 			String next = "";
-			
-			ArrayList<Integer> numberedValues = new ArrayList<Integer>();
 			
 			while(reader.ready()) {
 				next = "" + (char)reader.read();
@@ -40,19 +41,58 @@ public class Compression {
 				{
 					table.put(cAndN, number);
 					number++;
-					numberedValues.add(table.get(current));
+					asciiValues.add(table.get(current));
 					current = next;
 				}
 			}
-			numberedValues.add(table.get(current));
-			
-			for (int i = 0; i < numberedValues.size(); i++)
+			asciiValues.add(table.get(current));
+			reader.close();
+			return asciiValues;
+			/*for (int i = 0; i < asciiValues.size(); i++)
 			{
-				System.out.println (numberedValues.get(i));
+				System.out.println (asciiValues.get(i));
 			}
 			
-			reader.close();
+			reader.close();*/
+			
 		}
+	
+	public void decompress (ArrayList<Integer> encodedValues) throws IOException
+	{
+		HashMap<String,Integer> reconstructedDict = new HashMap <String,Integer>();
+		//initializes new dictionary
+		for (int i = 0; i<256; i++) {
+			reconstructedDict.put( "" + (char)i , i);
+		}
+		
+		int dictLength = 256;
+		BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/decompressedFile.txt"));
+		int firstVal = encodedValues.get(0);
+		String firstChar = "" + (char)(firstVal);
+		writer.write(firstChar);
+		for (int k = 1; k < encodedValues.size(); k++)
+		{
+			int val = encodedValues.get(k);
+			String current = "" + (char)(val);
+			String next = "";
+			String firstOfNext = "";
+			if (!reconstructedDict.containsKey(current))
+			{
+				next = firstChar;
+				next += firstOfNext;
+			}
+			else
+			{
+				next = current;
+				writer.write(next);
+				firstOfNext = ""+next.charAt(0);
+				reconstructedDict.put(firstChar, dictLength);
+				firstChar = current;
+			}
+		}
+		System.out.println ("File Decompressed");
+		writer.close();
+	}
 		
 		
 		/*
@@ -83,7 +123,8 @@ public class Compression {
 	
 	public static void main (String[] args) throws IOException {
 		Compression lolW = new Compression ();
-		lolW.compress(new File("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/lzw-file1.txt"));
+		ArrayList<Integer> result = lolW.compress(new File("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/lzw-file1.txt"));
+		lolW.decompress(result);
 	}
 	
 }
