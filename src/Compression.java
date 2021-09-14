98d1753d48cceb9572482af62d85837c4dbd3cae
Alex-Shane <ashane1230@gmail.com>
2021-09-14 09:52:58 -0700

    updated comments

--- src/Compression.java
+++ src/Compression.java
@@ -13,16 +13,18 @@ public class Compression {
 	
 	private HashMap<String, Integer> table = new HashMap<String, Integer>();
 	private ArrayList<Integer> asciiValues = new ArrayList<Integer>();
+	private File inputFile;
 	
-	public Compression () {
+	public Compression (File originalFile) {
 		// adds ascii table
 		for (int i = 0; i<256; i++) {
 			table.put( "" + (char)i , i);
 		}
+		inputFile = originalFile;
 	}
 	
 	@SuppressWarnings("deprecation")
-	public ArrayList<Integer> compress (File inputFile) throws IOException{
+	public ArrayList<Integer> compress () throws IOException{
 
 			BufferedReader reader = new BufferedReader (new FileReader(inputFile));
 			
@@ -67,26 +69,38 @@ public class Compression {
 		
 		int dictLength = 256;
 		BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/decompressedFile.txt"));
+		
+		//decodes first character of int array
 		int firstVal = encodedValues.get(0);
 		String firstChar = "" + (char)(firstVal);
 		writer.write(firstChar);
+		
+		//initializes values used in the for loop algorithm
 		String next = "";
 		String firstOfNext = "";
 		int oldVal = firstVal;
+		
+		//decompression algorithm
 		for (int k = 1; k < encodedValues.size(); k++)
 		{
 			int val = encodedValues.get(k);
 			String previous = reconstructedDict.get(oldVal);
+			
+			//check for if the String representation of value is not in the dictionary
 			if (val >= dictLength)
 			{
 				next = previous + firstOfNext;
 			}
+			
+			//if String representation of value is in dictionary, set next equal to the String mapped with val
 			else
 			{
 				next = reconstructedDict.get(val);
 			}
 			writer.write(next);
 			firstOfNext = "" + next.charAt(0);
+			
+			//puts previous+firstOfNext string into the dictionary
 			reconstructedDict.put(dictLength, previous+firstOfNext);
 			dictLength++;
 			oldVal = val;
@@ -95,36 +109,9 @@ public class Compression {
 		writer.close();
 	}
 		
-		
-		/*
-		byte[] byteArray = new byte[numberedValues.size()*3+10];
-		for (int i = 0; i< numberedValues.size(); i++) {
-			if (numberedValues.get(i)< 256) {
-				BigInteger bigInt = new BigInteger("" + numberedValues.get(i).intValue());
-				byte[] littleArr = bigInt.toByteArray();
-				byteArray[i*3] = 0; 
-				for(int j = 1; j<= littleArr.length; j++) {
-					byteArray[i*3+j] = littleArr[j];
-				}
-			}
-			else {
-				BigInteger bigInt = new BigInteger("" + numberedValues.get(i).intValue());
-				byte[] littleArr = bigInt.toByteArray();
-				for(int j = 0; j< littleArr.length; j++) {
-					byteArray[i*3+j] = littleArr[j];
-				}
-				
-			}
-		}
-		FileOutputStream writer = new FileOutputStream(outputFileName);
-		writer.write(byteArray);
-		
-		writer.close(); */
-	
-	
 	public static void main (String[] args) throws IOException {
-		Compression lolW = new Compression ();
-		ArrayList<Integer> result = lolW.compress(new File("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/lzw-file3.txt"));
+		Compression lolW = new Compression (new File("/Users/Alex/Desktop/Advanced-Topics-CS/LZW-Compression/lzw-file3.txt"));
+		ArrayList<Integer> result = lolW.compress();
 		lolW.decompress(result);
 	}
 	
